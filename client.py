5d5d3b6775cc63bf09dd5269454cb7364a50cb19
y2792li <y2792li@uwaterloo.ca>
2023-02-22 16:42:55 -0500

    complete all functions

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 from socket import *
 import sys
+import ipaddress
 
 
 # get an available port number if necessary
@@ -13,22 +14,24 @@ def get_r_port():
 
 # check the format of argv command
 def checker():
-    if len(sys.argv) != 6:
-        print('Usage: \'python client.py <server address> <n_port> <mode> <req_code> <file_received>\'')
+    try:
+        ipaddress.ip_address(server_address)
+    except ValueError:
+        print('address/netmask is invalid: %s' % server_address)
         sys.exit(1)
     try:
-        int(sys.argv[2])
+        int(n_port)
     except ValueError:
         print("n_port must be an integer.")
         sys.exit(1)
-    if int(sys.argv[2]) < 1024 or int(sys.argv[2]) > 65535:
+    if int(n_port) < 1024 or int(n_port) > 65535:
         print("n_port must be between [1024, 65535].")
         sys.exit(1)
-    if sys.argv[3] != 'P' and sys.argv[3] != 'A':
+    if mode != 'P' and mode != 'A':
         print("mode should be either 'P' or 'A'.")
         sys.exit(1)
     try:
-        int(sys.argv[4])
+        int(req_code)
     except ValueError:
         print("req_code must be an integer.")
         sys.exit(1)
@@ -55,13 +58,36 @@ def handle(ip, port, m, req):
                     if not data:
                         break
                     f.write(data)
-    # elif m == 'P':  # Passive mode
+    elif m == 'P':  # Passive mode
+        message = "PASV " + str(req)
+        udp_sock.sendto(message.encode(), (ip, int(port)))
+        acknowledgement, s_address = udp_sock.recvfrom(2048)
+        r_port = int(acknowledgement.decode())
+        if r_port == 0:
+            sys.exit(1)  # wrong req_code leads to system exiting gracefully
+        else:
+            tcp_sock = socket(AF_INET, SOCK_STREAM)
+            tcp_sock.connect((ip, r_port))  # init the connection with server
+            with open(file_received, 'wb') as f:
+                while True:
+                    data = tcp_sock.recv(1024)
+                    if not data:
+                        break
+                    f.write(data)
+    else:
+        print("exception")
+        sys.exit(1)
 
 
-checker()
+if len(sys.argv) != 6:
+    print('Usage: \'python client.py <server address> <n_port> <mode> <req_code> <file_received>\'')
+    sys.exit(1)
 server_address = sys.argv[1]
+if server_address == 'localhost':
+    server_address = '127.0.0.1'
 n_port = sys.argv[2]
 mode = sys.argv[3]
 req_code = sys.argv[4]
 file_received = sys.argv[5]
+checker()
 handle(server_address, n_port, mode, req_code)
--- server.py
+++ server.py
@@ -25,7 +25,7 @@ def udp_socket(s_port):
     server_sock = socket(AF_INET, SOCK_DGRAM)
     try:
         server_sock.bind(('', s_port))
-    except error as e:
+    except error:
         server_sock.bind(('', 0))
         s_port = server_sock.getsockname()[1]
     server_sock.close()
@@ -33,38 +33,60 @@ def udp_socket(s_port):
     return s_port
 
 
+def handle(req_code, file_name, n_port):
+    udp_sock = socket(AF_INET, SOCK_DGRAM)
+    udp_sock.bind(('', n_port))
+
+    while True:
+        message, client_address = udp_sock.recvfrom(2048)
+        msg_arr = message.split()
+
+        if msg_arr[0].decode() == "PORT":  # Active mode
+            if len(msg_arr) != 3:
+                print("client's sending format is wrong")
+                sys.exit(1)
+            if msg_arr[2].decode() != req_code:  # wrong req_code
+                # print("deny for transfer")
+                udp_sock.sendto("0".encode(), client_address)
+            else:
+                print("ok for transfer")
+                udp_sock.sendto("1".encode(), client_address)
+                tcp_sock = socket(AF_INET, SOCK_STREAM)
+                # tcp_port = get_r_port()
+                # tcp_sock.bind('', tcp_port)
+                # tcp_sock.listen(1)
+                tcp_sock.connect((client_address[0], int(msg_arr[1].decode())))
+                with open(file_name, 'rb') as f:
+                    data = f.read(1024)
+                    while data:
+                        tcp_sock.sendall(data)
+                        data = f.read(1024)
+                tcp_sock.close()
+
+        elif msg_arr[0].decode() == "PASV":  # Active mode
+            if len(msg_arr) != 2:
+                print("client's sending format is wrong")
+                sys.exit(1)
+            if msg_arr[1].decode() != req_code:  # wrong req_code
+                udp_sock.sendto("0".encode(), client_address)
+            else:
+                tcp_port = get_r_port()
+                tcp_sock = socket(AF_INET, SOCK_STREAM)
+                tcp_sock.bind(('', tcp_port))
+                tcp_sock.listen(1)
+                udp_sock.sendto(str(tcp_port).encode(), client_address)
+                conn_sock, addr = tcp_sock.accept()
+                with open(file_name, 'rb') as f:
+                    data = f.read(1024)
+                    while data:
+                        conn_sock.sendall(data)
+                        data = f.read(1024)
+                conn_sock.close()
+
+
 checker()  # check for argv format
 sever_port = 5001
-req_code = sys.argv[1]
-file_name = sys.argv[2]
-n_port = udp_socket(sever_port)  # get the n_port
-udp_sock = socket(AF_INET, SOCK_DGRAM)
-udp_sock.bind(('', n_port))
-
-while True:
-    message, clientAddress = udp_sock.recvfrom(2048)
-    msg_arr = message.split()
-    print(str(msg_arr))
-    if len(msg_arr) != 3:
-        print("client's sending format is wrong")
-        sys.exit(1)
-    if msg_arr[0].decode() == "PORT":  # Active mode
-        if msg_arr[2].decode() != req_code:  # wrong req_code
-            print("deny for transfer")
-            udp_sock.sendto("0".encode(), clientAddress)
-        else:
-            print("ok for transfer")
-            udp_sock.sendto("1".encode(), clientAddress)
-            tcp_sock = socket(AF_INET, SOCK_STREAM)
-            # tcp_port = get_r_port()
-            # tcp_sock.bind('', tcp_port)
-            # tcp_sock.listen(1)
-            tcp_sock.connect((clientAddress[0], int(msg_arr[1].decode())))
-            with open(file_name, 'rb') as f:
-                data = f.read(1024)
-                while data:
-                    tcp_sock.sendall(data)
-                    data = f.read(1024)
-            tcp_sock.close()
-    else:
-        print("exception")
+r_code = sys.argv[1]
+f_name = sys.argv[2]
+port = udp_socket(sever_port)  # get the n_port
+handle(r_code, f_name, port)
